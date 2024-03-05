4122cdadaa98cf61c3abb2b5a63b6ec3ddddb2f2
thisan2005 <153266193+thisan2005@users.noreply.github.com>
2024-03-05 18:41:05 +0530

    New node process

--- backend/movie-locations.py
+++ backend/movie-locations.py
@@ -1,37 +1,71 @@
 import sys
-from bs4 import BeautifulSoup
 import requests
+import pymongo
 
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+# Connect to MongoDB
+uri = "mongodb+srv://danindu:gLe8lKyg5S0UNRP4@jetvialensedb.x7bdmb1.mongodb.net/?retryWrites=true&w=majority&appName=JetViaLenseDB"
+client = MongoClient(uri)
+db = client['film-data']
+collection = db['locations']
+
+# check connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# getting movie locations by passing id as argument from node
 movieID = sys.argv[1]
+# movieID = "tt1670345"
 
-def predit():
+duplicationCount = collection.count_documents({"imdbID": movieID})
+print("Duplication count: ", duplicationCount)
 
-    # movieID=input("enter the movie ID : ")
-    # movieId = "tt1670345"
+if(duplicationCount == 0):
+    #def predit():
 
-    url = "https://www.imdb.com/title/"+movieID+"/locations/?ref_=tt_dt_loc"
+        url = "https://www.imdb.com/title/"+movieID+"/locations/?ref_=tt_dt_loc"
 
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-    # Find the div with the specified class
-        div_elements = soup.find_all('div', {'class': 'sc-43930a5b-2 lkBRTN'})
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        locations = []  # List to store locations
+            # Find the div with the specified class
+            div_elements = soup.find_all('div', {'class': 'sc-43930a5b-2 lkBRTN'})
 
-        for div_element in div_elements:
-            location = div_element.text.strip()  # Get the text content of the div and remove leading/trailing whitespace
-            locations.append(location)  # Append location to the list
-            print(location)
-    else:
-        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+            locations = []  # List to store locations
 
+            for div_element in div_elements:
+                location = div_element.text.strip()  # Get the text content of the div and remove leading/trailing whitespace
+                locations.append(location)  # Append location to the list
 
-    return {"location":response}
+            # adding to mongodb
+            if(len(locations) == 0):
+                locations.append("No locations found for this movie")
+            else:
+                result = collection.insert_one({
+                        "imdbID": movieID,
+                        "movieTitle": "TEST MOVIE TITLE",
+                        "locations": locations
+                    })
+            # print("data ID: ", result.inserted_id)
+            
 
-predit()
+            for data in locations:
+                print(data)
+
+        else:
+            print(f"Failed to load the locations. Status code: {response.status_code}")
+
+        #return {"location":response}
+
+#predit()
