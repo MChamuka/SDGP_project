5d7abbe9197344e21a00d1a21499d6f64852640d
Vidunaka <158425773+Vidunaka@users.noreply.github.com>
2024-03-08 23:12:41 +0530

    SignPage

--- FrontEnd_react/src/pages/signUp.js
+++ FrontEnd_react/src/pages/signUp.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+import Validation from './LoginValidation';
 
-function signUp() {
+function Signup() {
+
+    const[values,setValues] = useState({
+        name:'',
+        email: '',
+        password: '' 
+    })
+    const [errors, setErrors] = useState({})
+    const handleInput = (event) => {
+        setValues(prev =>({...prev, [event.target.name]: [event.target.value]}) )
+    }
+    const handleSubmit =(event) =>{
+        event.preventDefault();
+        setErrors(Validation(values));
+    }
+
   return (
-    <div>signUp</div>
+    <div className='a1'>
+        <div className='a2'>
+            <h2>Sign Up</h2>
+            <form action='' onSubmit={handleSubmit}>
+                <div className='form'>
+                    <label htmlFor='name'><strong> First Name</strong></label>
+                    <input type='text' placeholder='Enter First Name' name='name'
+                    onChange={handleInput} className='' required></input>
+                    {errors.name && <span className=''> {errors.name}</span>}
+                </div>
+                <div className='form'>
+                    <label htmlFor='Email'><strong>Email</strong></label>
+                    <input type='email' placeholder='Enter Email' name='email'
+                    onChange={handleInput} className='' required></input>
+                    {errors.email && <span className=''> {errors.email}</span>}
+                </div>
+                <div className='form'>
+                    <label htmlFor='password'><strong>Password</strong></label>
+                    <input type='password' placeholder='Enter Password'name='password'
+                    onChange={handleInput} className='' required></input>
+                    {errors.password && <span className=''> {errors.password}</span>}
+                </div>
+                <button type='submit' className='Button'>Sign up</button>
+                <p> You are agree to our terms and policies</p>
+                <Link to="/"  className='btn'>Login</Link> 
+
+            </form>
+        </div>
+    </div>
   )
 }
 
-export default signUp
+export default Signup
