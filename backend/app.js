499a2c24b9af9c268ac64e149816392f452d146a
thisan2005 <153266193+thisan2005@users.noreply.github.com>
2024-03-09 10:13:05 +0530

    Data reading error handled

--- backend/app.js
+++ backend/app.js
@@ -21,8 +21,7 @@ mongoose.connect(dbURI)
     .then(() => console.log('connected'))
     .catch((err) => console.log(err));
 
-let receivedID;
-
+    
 // get movie ids to the backend
 app.post('/locations', (req, res) => {
   movieData = req.body;
@@ -31,56 +30,34 @@ app.post('/locations', (req, res) => {
     console.log('Locations stored');
     getLocations(movieData);
   });
-
-  // setTimeout(getLocations(movieData), 3000)
 });
 
-// GET LOCATIONS FROM THE DATABASE (PROBLEM - CANNOT ACCESS THE DATA FROM THE POST METHOD IN OTHER METHODS))
-
-// send location data to react
-// app.get('/locations', (req, res) => {
-
-//   // let ID = 'tt1677720';
-//   // ID = receivedID.ID;
-
-
-//   // Location.find({imdbID: id}, {"_id": 1, "locations": 1})
-//   //   .then((result) => {
-//   //     res.send(result);
-//   //     console.log(result);
-//   //   })
-//   //   .catch((err) => {
-//   //     console.log(err);
-//   //   });
-  
-//   // res.send(receivedID.ID);
-// });
-
-
-// get locations from the database
+// store locations in the database
 async function storeLocations() {
-  const args = [movieData.ID];
+  const id = [movieData.ID];
+  const title = [movieData.Title];
 
   // executing the python script
-  const pythonProcess = spawn('python', ['movie-locations.py', ...args]);
+  const pythonProcess = spawn('python', ['movie-locations.py', id, title]);
 
   pythonProcess.stderr.on('data', (data) => {
     console.error(`Python script error: ${data.toString()}`);
   });
-
-  // getLocations(movieData);
 }
 
+// get locations from the database
 function getLocations () {
 
   id = movieData.ID;
   title = movieData.Title;
 
   app.get('/locations', (req, res) => {
-    Location.find({imdbID: id}, {"_id": 1, "locations": 1})
+    Location.find({imdbID: id}, {"_id": 1, "movieTitle": 1, "locations": 1})
       .then((result) => {
+        if(result.length != 0) {
+          console.log('Data recieved');
+        }
         res.send(result);
-        console.log(result);
       })
       .catch((err) => {
         console.log(err);
--- backend/movie-locations.py
+++ backend/movie-locations.py
@@ -5,7 +5,7 @@ import pymongo
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-# Connect to MongoDB
+# connect to MongoDB
 uri = "mongodb+srv://danindu:gLe8lKyg5S0UNRP4@jetvialensedb.x7bdmb1.mongodb.net/?retryWrites=true&w=majority&appName=JetViaLenseDB"
 client = MongoClient(uri)
 db = client['film-data']
@@ -18,15 +18,13 @@ try:
 except Exception as e:
     print(e)
 
-# getting movie locations by passing id as argument from node
+# getting movie locations by passing id and title as arguments from node
 movieID = sys.argv[1]
-# movieID = "tt1670345"
+movieTitle = sys.argv[2]
 
 duplicationCount = collection.count_documents({"imdbID": movieID})
-print("Duplication count: ", duplicationCount)
 
 if(duplicationCount == 0):
-    #def predit():
 
         url = "https://www.imdb.com/title/"+movieID+"/locations/?ref_=tt_dt_loc"
 
@@ -48,24 +46,23 @@ if(duplicationCount == 0):
                 location = div_element.text.strip()  # Get the text content of the div and remove leading/trailing whitespace
                 locations.append(location)  # Append location to the list
 
-            # adding to mongodb
+            # adding to the mongodb (document)
             if(len(locations) == 0):
-                locations.append("No locations found for this movie")
+                locations.append("No locations found for this movie: click here to submit a location.")
+                result = collection.insert_one({
+                        "imdbID": movieID,
+                        "movieTitle": movieTitle,
+                        "locations": locations
+                    })
             else:
                 result = collection.insert_one({
                         "imdbID": movieID,
-                        "movieTitle": "TEST MOVIE TITLE",
+                        "movieTitle": movieTitle,
                         "locations": locations
-                    })
-            # print("data ID: ", result.inserted_id)
-            
+                    })           
 
             for data in locations:
                 print(data)
 
         else:
             print(f"Failed to load the locations. Status code: {response.status_code}")
-
-        #return {"location":response}
-
-#predit()
