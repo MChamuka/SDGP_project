97428a5dc019fe6558f3016db8dbbfdfbcbaa2d5
Vidunaka <158425773+Vidunaka@users.noreply.github.com>
2024-03-23 13:52:12 +0530

    connect data gathering form

--- FrontEnd_react/src/index.js
+++ FrontEnd_react/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SignUp from './pages/signUp.js';
 import Locations from './pages/Locations.js';
-import Form  from './pages/Form.js';
+import MovieForm from './pages/UserProfilePage/UserProfileComponents/Feaatures/DataGatheringFormFeature/MovieForm.js';
 import Profile from "./pages/UserProfilePage/Profile.js";
 
 
@@ -21,7 +21,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
       },
       {
         path: "Form",
-        element: <Form /> ,
+        element: <MovieForm /> ,
       },
       {
         path: "profile",
