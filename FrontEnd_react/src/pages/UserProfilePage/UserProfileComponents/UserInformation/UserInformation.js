b7c9ad355cfa4ce5782a160978e6e705763f5915
TilanFernando <152051887+TilanFernando@users.noreply.github.com>
2024-03-24 22:41:38 +0530

    made user profile page responsive

--- FrontEnd_react/src/pages/UserProfilePage/UserProfileComponents/UserInformation/UserInformation.js
+++ FrontEnd_react/src/pages/UserProfilePage/UserProfileComponents/UserInformation/UserInformation.js
@@ -1,28 +1,28 @@
-import React, { useState, useRef,useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./userInformation.css";
-import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 //import 'bootstrap/dist/css/bootstrap.min.css'
-
 
 export default function UserInformation() {
   const [profileImage, setProfileImage] = useState("/Images/1.jpg");
   const fileInputRef = useRef(null);
 
-  const [users,setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   const openStreamlitApp = () => {
-    window.open('http://localhost:8501'); //streamlt URL
+    window.open("http://localhost:8501"); //streamlt URL
   };
 
-  useEffect(()=>{
-    axios.get('http://localhost:4000/getUsers')
-    .then(users=> {setUsers(users.data[0])
-      
-      console.log(users.data[0].firstName)
-    })
-    .catch(err=>console.log(err))
-  },[])
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/getUsers")
+      .then((users) => {
+        setUsers(users.data[0]);
+
+        console.log(users.data[0].firstName);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
@@ -55,7 +55,9 @@ export default function UserInformation() {
         </div>
         <div className="featureContainer">
           <div className="userProfilefeatures">
-            <button className="feature" onClick={openStreamlitApp}>Recommended Movies</button>
+            <button className="feature" onClick={openStreamlitApp}>
+              Recommended Movies
+            </button>
             <button className="feature">Data Gathering Form</button>
             <button className="feature">Journal</button>
             <button className="feature">Achievements</button>
